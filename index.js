b5bf316fb7a6d6a0d4f0122d5b86e789960b576c
Sujay V <sujaywebdev@gmail.com>
2022-12-22 13:59:14 +0530

    using EJS for UI HTML

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const express = require('express');
 
 const app = express();
 
+app.set("view engine","ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get("/myget",(req,res) => {
     console.log(req.body);
 
     res.send(req.body)
+})
+
+app.get("/myformget",(req,res) => {
+    
+    res.render("getform")
+})
+
+app.get("/myformpost",(req,res) => {
+    res.render("postform")
 })
 
 app.listen(4000,() => {
