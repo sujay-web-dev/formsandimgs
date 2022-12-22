e9aff9f2a5399e2345af7069e296ae9ce0c8ef2f
Sujay V <sujaywebdev@gmail.com>
2022-12-22 12:24:29 +0530

    Initial Commit , with basic setup to handle Images & Forms

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/myget",(req,res) => {
+    console.log(req.body);
+
+    res.send(req.body)
+})
+
+app.listen(4000,() => {
+    console.log(`Server is RUNNING at Port 4000`);
+})
